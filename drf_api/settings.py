9ecb7addb100acb4223c6690b549edd4a9ed16a0
mr-fibonacci <adamlapinski1@gmail.com>
2021-06-18 15:02:47 +0000

    003.2 started project, added profiles app - fixed cloudinary typo - db gitignored

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -17,7 +17,7 @@ if os.path.exists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = {
-    'CLOUDINARY_URL': os.environ.get('CLOURINARY_URL')
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
