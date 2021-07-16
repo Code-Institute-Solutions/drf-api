3748ed4d93b45dcabdbf0b29b95be27ad644fe2d
mr-fibonacci <adamlapinski1@gmail.com>
2021-07-16 12:07:46 +0000

    011.2 CommentDetailSerializer challenge

--- comments/serializers.py
+++ comments/serializers.py
@@ -22,3 +22,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
