d5b167df08db8ef479633393334783af2821f364
mr-fibonacci <adamlapinski1@gmail.com>
2021-07-19 12:24:07 +0000

    013.2 LikeSerializer challenge

---
+++ likes/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
