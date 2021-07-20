a918da6065ef5c399ff7655638960c2628af83d4
mr-fibonacci <adamlapinski1@gmail.com>
2021-07-20 17:39:30 +0000

    013.3 Handling duplicate likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
@@ -12,3 +13,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
