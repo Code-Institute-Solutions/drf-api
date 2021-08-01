cebe1ac2b4daa808ec0c33af33b501864f5b25f0
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-01 20:00:34 +0000

    019.1 Posts text search feature

--- posts/views.py
+++ posts/views.py
@@ -17,7 +17,12 @@ class PostList(generics.ListCreateAPIView):
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
     ]
     ordering_fields = [
         'likes_count',
