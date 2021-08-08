717985cababa95b44b468f7560a3692601497c1e
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-08 18:06:09 +0000

    019.3 Filtering profiles with DjangoFilterBackend challenge

--- profiles/views.py
+++ profiles/views.py
@@ -23,6 +23,7 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'posts_count',
