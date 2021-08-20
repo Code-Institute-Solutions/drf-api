5210e34d25111e1556d10e895206e255d990e4bb
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-20 08:34:56 +0000

    Added a custom logout route - dj-rest-auth lib bug, issue open on github - samesite attribute not being passed to the logout route

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    # our logout route has to be above the default one to be matched first
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
--- drf_api/views.py
+++ drf_api/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -7,3 +11,28 @@ def root_route(request):
     return Response({
         "message": "Welcome to my drf API!"
     })
+
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
